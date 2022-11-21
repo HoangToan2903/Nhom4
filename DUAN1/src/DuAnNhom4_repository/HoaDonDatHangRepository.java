5b2052876eeb14735ed60f98d3c1dff33e3139f5
LAPTOP-N6506U5Q\TRAN VAN DUONG <duongtvph19943@fpt.edu.vn>
2022-11-21 11:31:52 +0700

    update_repo

--- DUAN1/src/DuAnNhom4_repository/HoaDonDatHangRepository.java
+++ DUAN1/src/DuAnNhom4_repository/HoaDonDatHangRepository.java
@@ -19,31 +19,6 @@ import java.util.ArrayList;
  * @author TRAN VAN DUONG
  */
 public class HoaDonDatHangRepository {
-
-//    public static final Connection con = DBContext.getConnection();
-////     public List<GiangVien> getALL() {
-//        //b1:viết query
-//        String query = "SELECT * FROM giang_vien";
-//        //b2:mở cổng kết nối
-//        try (Connection con = SQLsever_Connection.getConnection();
-//                PreparedStatement ps = con.prepareStatement(query)) {
-//            //b3:statement: cho phép gửi các câu SQL tới DATABASE
-//            //b4:  khởi tạo kiểu trả về của hàm
-//            List<GiangVien> listGV = new ArrayList<>();
-//            //b5:resultset: đại điện cho tập hipw cá bản ghi (record)
-//            //có được sau kho thực hiện truy vấn (query)
-//            ResultSet rs = ps.executeQuery();
-//            //b6: đọc DATA
-//            while (rs.next()) {
-//                GiangVien gv = new GiangVien(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getBoolean(6));
-//                listGV.add(gv);
-//            }
-//            return listGV;
-//        } catch (SQLException ex) {
-//            // class này xử ký bất cứ lỗi nào xuất
-//            ex.printStackTrace(System.out);
-//        }
-//        return null;
     public static List<vHoaDonDatHang> getAll() {
         String query = "select SANPHAM.TENSP, HOADONDATHANG.SOLUONG, SANPHAM.GIABAN, SANPHAM.MAUSAC, SANPHAM.SIZE, KHACHHANG.HOTEN,SANPHAM.HINHANH, HOADONDATHANG.TRANGTHAI\n" +
 "                from HOADONDATHANG " +
@@ -64,19 +39,6 @@ public class HoaDonDatHangRepository {
             return null;
         }
     }
-    //        public boolean delete(String maGV) {
-    //        String query = "DELETE FROM [dbo].[giang_vien]"
-    //                + "      WHERE ma_gv = ?";
-    //        int check = 0;
-    //        try (Connection con = SQLsever_Connection.getConnection();
-    //                PreparedStatement ps = con.prepareStatement(query)) {
-    //            ps.setObject(1, maGV);
-    //            check = ps.executeUpdate();
-    //        } catch (SQLException ex) {
-    //            ex.printStackTrace(System.out);
-    //        }
-    //        return check > 0;
-    //        //    }
         public boolean delete(String TenSanPham) {
             String query = "DELETE FROM [dbo].[HOADONDATHANG]"
                     + "      WHERE TENSANPHAM = ?";
@@ -90,25 +52,6 @@ public class HoaDonDatHangRepository {
             return check > 0;
         }
 
-    //    public static void main(String[] args) throws SQLException {
-    //        HoaDonDatHangRepository hoaDonDatHangRepository = new HoaDonDatHangRepository();
-    //        hoaDonDatHangRepository.getAll();
-    //    }
-    //    public GiangVien getOne(String maGV) {
-    //        String query = "select * from giang_vien where ma_gv = ?";
-    //        try (Connection con = SQLsever_Connection.getConnection();
-    //                PreparedStatement ps = con.prepareStatement(query)) {
-    //            ps.setObject(1, maGV);
-    //            ResultSet rs = ps.executeQuery();
-    //            if (rs.next()) {
-    //                return new GiangVien(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getBoolean(6));
-    //            }
-    //        } catch (SQLException ex) {
-    //            // class này xử ký bất cứ lỗi nào xuất
-    //            ex.printStackTrace(System.out);
-    //        }
-    //        return null;
-    //    }
     public vHoaDonDatHang timKiem(int TrangThai) {
         String query = "select SANPHAM.TENSP, HOADONDATHANG.SOLUONG, SANPHAM.GIABAN, SANPHAM.MAUSAC, SANPHAM.SIZE, KHACHHANG.HOTEN,SANPHAM.HINHANH, HOADONDATHANG.TRANGTHAI\n" +
 "                from HOADONDATHANG " +
